64eacbd863b7fc2e41522cc956d95d60f32b784e
Chris Bourke <cbourke@cse.unl.edu>
2018-02-25 23:43:22 -0600

    updated source file, removed old readme

--- hello.c
+++ hello.c
@@ -1,12 +1,16 @@
 /**
-  Author: Chris Bourke
-  Date: xx/xx/20xx
-  Hello World in C 
-*/ 
+ * Author: Chris Bourke
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
 
-#include <stdio.h> 
- 
 int main(int argc, char **argv) {
+
   printf("Hello World!\n");
+
   return 0;
 }
